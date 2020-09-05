c99dcead8a9fdc80c62c4f1796cbf69953dc32e4
Giulio Martena <giuliomariomartena@gmail.com>
2020-09-05 21:09:08 +0200

    Ass2 Task2

---
+++ models/leaders.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const leaderSchema = new Schema({
+    "name": {
+        type: String,
+        required: true
+    },
+    "image": {
+        type: String,
+        required: true
+    },
+    "designation": {
+        type: String,
+        required: true
+    },
+    "abbr": {
+        type: String,
+        required: true
+    },
+    "description": {
+        type: String
+    },
+    "featured": {
+        type: Boolean,
+        default: false
+    }
+})
+
+var Leaders = mongoose.model('Leaders', leaderSchema);
+
+module.exports = Leaders;
--- routes/leaderRouter.js
+++ routes/leaderRouter.js
@@ -1,45 +1,87 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const leaderRouter = express.Router();
+
+const Leaders = require('../models/leaders');
 
 leaderRouter.use(bodyParser.json());
 
 leaderRouter.route('/')
-           .all((req,res,next) => {
-               res.statusCode = 200;
-               res.setHeader('Content-Type', 'text/plain');
-               next();
-           })
            .get((req,res,next) => {
-               res.end('Will fetch the leaders for you');
+               Leaders.find({})
+                      .then(leaders => {
+                          res.statusCode = 200;
+                          res.setHeader('Content-Type','application/json');
+                          res.json(leaders);
+                      }, err => next(err))
+                      .catch(err => next(err));
            })
            .post((req,res,next) => {
-               res.write('Posting new leader...\n');
-               res.end('Posted leader:\n' + req.body.name + '\n' + req.body.description);
+               Leaders.create(req.body)
+                      .then(
+                          leader => {
+                              res.statusCode = 200;
+                              res.setHeader('Content-Type', 'application/json');
+                              res.json(leader);
+                          }, err => next(err))
+                      .catch(err => next(err));
            })
            .put((req,res,next) => {
                res.statusCode = 403;
-               res.end('PUT operation not permitted on endpoint ' + req.path);
+               res.end('PUT operation not permitted on endpoint /leaders');
            })
            .delete((req,res,next) => {
-               res.end('This will delete all the leaders.');
+               Leaders.deleteMany({})
+                      .then(leader => {
+                          res.statusCode = 200;
+                          res.setHeader('Content-Type','application/json');
+                          res.json(leader);
+                      }, err => next(err))
+                      .catch(err => next(err))
            })
 
 leaderRouter.route('/:leaderId')
            .get((req,res,next) => {
-               res.end('Fetching leader with id ' + req.params.leaderId);
+               Leaders.findById(req.params.leaderId)
+                      .then(leader => {
+                        switch(leader) {
+                            case null:
+                                err = new Error('Leader ' + req.params.leaderId + ' not found');
+                                err.status = 404;
+                                return next(err);
+                            default:
+                                res.statusCode = 200;
+                                res.setHeader('Content-Type', 'application/json');
+                                res.json(leader);
+                        }
+                      })
+                      .catch(err => next(err));
            })
            .post((req,res,next) => {
                res.statusCode = 403;
                res.end('POST operation not allowed on single leader.\nA friendly reminder that the id gets assigned automatically');
            })
            .put((req,res,next) => {
-               res.write('Updating leader with id ' + req.params.leaderId);
-               res.end('Updated leader:\nName: ' + req.body.name + '\nDescription: ' + req.body.description);
+               Leaders.findByIdAndUpdate(req.params.leaderId, {
+                   $set: req.body
+               }, { new: true })
+                   .then(leader => {
+                       res.statusCode = 200;
+                       res.setHeader('Content-Type', 'application/json');
+                       res.json(leader);
+                   }, err => next(err))
+                   .catch(err => next(err));
            })
            .delete((req,res,next) => {
-               res.end('Deleting leader with ID ' + req.params.leaderId);
+               Leaders.findByIdAndDelete(req.params.leaderId)
+                   .then(result => {
+                       res.statusCode = 200;
+                       res.setHeader('Content-Type', 'application/json');
+                       res.json(result);
+                   }, err => next(err))
+                   .catch(err => next(err))
            });
 
 module.exports = leaderRouter;
